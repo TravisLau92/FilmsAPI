ecf08861f98c596a82a24fe2236fbe24eaeb58d3
Travis Lau <travislau92@hotmail.com>
2024-07-30 14:56:25 +0000

    updated index.html and script.js

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-//Get all the DOM elements from the index html
-
 const inputNumberElement = document.getElementById('inputNum');
 const searchButtonElement = document.getElementById('searchButton');
 const resultContainerElement = document.getElementById('result-container');
 
-//Function to fetch film from API
 
 async function fetchFilms(filmId) {
-  const response = await fetch(`https://swapi.dev/api/films/${filmId}/`);
+  const response = await fetch(`https://swapi.dev/api/films/${filmId}`);
   const data = await response.json();
   return data;
 }
 
-// When search button is clicked, triggers the callback function below
-
 searchButtonElement.addEventListener('click', async () => {
+const inputNumberValue = inputNumberElement.value;
 
-  const inputNumberValue = inputNumberElement.value;
+const filmsData = await fetchFilms(inputNumberValue);
 
-  const filmsData = await fetchFilms(inputNumberValue);
-
-  resultContainerElement.innerHTML = `
+resultContainerElement.innerHTML = `
   <h1>${filmsData.title}</h1>
   <p>Director: ${filmsData.director}</p>
   <p>Opening Crawl: ${filmsData.opening_crawl}</p>
